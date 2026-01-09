806dc746490704a860a5e5d8d4907faf4f6c851b
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 18:39:07 +0530

    add correct values

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-00ebc2f65b7ba8fc9"  # Replace with your subnet ID
+  subnet_id     = "subnet-071467d125ce00f81"  # Replace with your subnet ID
 
   tags = {
     Name = "MyInstance"
