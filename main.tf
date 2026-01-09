62bfd14920758db38f120e95a4ed436e793784b7
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 18:41:40 +0530

    add and create the vpc in my account

--- main.tf
+++ main.tf
@@ -2,13 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "sg" {
+  name        = "my-sg"
+  description = "Allow SSH inbound"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 instance
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-071467d125ce00f81"  # Replace with your subnet ID
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = "MyInstance"
   }
 }
-
