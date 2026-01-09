b2db17436dacaabc3ed8bc43815321051cab1d3d
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 19:02:18 +0530

    modify the main.tf file

--- main.tf
+++ main.tf
@@ -1,34 +1,61 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAYZIOXUTY4UAFWK6Y"
-  secret_key = "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"
+  region = "us-east-1"  # Change this if you want a different region
 }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "MyVPC"
+    Name = "my_vpc"
   }
 }
 
-# Create a subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+# Create public subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
-
   tags = {
-    Name = "MySubnet"
+    Name = "my_subnet"
   }
 }
 
-# Create a security group
-resource "aws_security_group" "sg" {
-  name        = "my-sg"
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my_route_table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "my_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Security Group allowing SSH
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
   description = "Allow SSH inbound"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 22
@@ -43,16 +70,21 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-# Create EC2 instance
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "MyInstance"
+    Name = "my_sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami                    = "ami-05fa00d4c63e32376" # Amazon Linux 2 (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  key_name               = "MyKeyPair" # Replace with your AWS key pair
+
+  tags = {
+    Name = "my_instance"
   }
 }
