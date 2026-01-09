2d744f567dabd4fc097bc929c6169e2239314d79
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 19:06:46 +0530

    add code

--- main.tf
+++ main.tf
@@ -1,71 +1,41 @@
 provider "aws" {
-<<<<<<< HEAD
-  region = "us-east-1"  # Change this if you want a different region
-=======
   region = "us-east-1"
-  access_key = "AKIAYZIOXUTY4UAFWK6Y"
-  secret_key = "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"
->>>>>>> 116e464e3902649afd538101e284bd04df840af8
 }
 
-# Create VPC
+# Create a VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "my_vpc"
+    Name = "TerraformPOCVPC"
   }
 }
 
-# Create public subnet
+# Create a subnet
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
   tags = {
-    Name = "my_subnet"
+    Name = "TerraformPOCSubnet"
   }
 }
 
-# Create Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my_igw"
-  }
-}
-
-# Create Route Table
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "my_route_table"
-  }
-}
-
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "my_assoc" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
-}
-
-# Security Group allowing SSH
+# Create a security group
 resource "aws_security_group" "my_sg" {
-  name        = "my_sg"
-  description = "Allow SSH inbound"
+  name        = "TerraformPOCSG"
+  description = "Allow SSH and HTTP"
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -76,21 +46,17 @@ resource "aws_security_group" "my_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "my_sg"
-  }
 }
 
-# EC2 Instance
-resource "aws_instance" "my_instance" {
-  ami                    = "ami-05fa00d4c63e32376" # Amazon Linux 2 (us-east-1)
+# Create an EC2 instance
+resource "aws_instance" "foo" {
+  ami                    = "ami-05fa00d4c63e32376"
   instance_type          = "t2.micro"
+  key_name               = "terraform_key_pair"          # Replace with your AWS key pair
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
-  key_name               = "MyKeyPair" # Replace with your AWS key pair
 
   tags = {
-    Name = "my_instance"
+    Name = "TerraformPOCInstance"
   }
 }
