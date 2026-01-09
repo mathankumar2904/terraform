98b93803c054708830cb66c201edec9d28fbba90
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 18:05:34 +0530

    Initial clean terraform commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TF-Instance"
+  }
+}
+
