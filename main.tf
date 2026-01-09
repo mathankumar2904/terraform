5dc43c869b761b39349d094ec588bf9d07c04d68
mathankumar <mathankumarg.softsuave@gmail.com>
2026-01-09 18:36:41 +0530

    add the main.tf code

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-00ebc2f65b7ba8fc9"  # Replace with your subnet ID
 
   tags = {
-    Name = "TF-Instance"
+    Name = "MyInstance"
   }
 }
 
